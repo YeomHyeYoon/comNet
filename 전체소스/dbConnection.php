d592304ad814cf3c1c7eec7475b9cc9b1eaa01d9
YeomHyeYoon <hyeyoon5019@naver.com>
2020-12-22 02:01:22 +0900

    전체소스삭제

--- 전체소스/dbConnection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("","","","");
-    mysqli_query($con, "set session character_set_connection=utf8;");
-    mysqli_query($con, "set session character_set_results=utf8;");
-    mysqli_query($con, "set session character_set_client=utf8;");
-?>
